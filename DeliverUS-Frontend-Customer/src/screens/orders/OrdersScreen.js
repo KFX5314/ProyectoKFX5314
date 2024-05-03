bf5fb46bbd1e93840635c0b235127908859f1cb6
Álvaro Luque <alvluqbuz@alum.us.es>
2024-05-03 19:02:23 +0200

    OrderDate papu

--- DeliverUS-Frontend-Customer/src/screens/orders/OrdersScreen.js
+++ DeliverUS-Frontend-Customer/src/screens/orders/OrdersScreen.js
@@ -7,13 +7,17 @@ import { brandPrimary, brandPrimaryTap } from '../../styles/GlobalStyles'
 
 import { AuthorizationContext } from '../../context/AuthorizationContext'
 import { getAll } from '../../api/OrderEndpoints'
-import { showMessage } from 'react-native-flash-message'
+import { showMessage } from 'react-native-flash-message';
 import * as GlobalStyles from '../../styles/GlobalStyles'
 import ImageCard from '../../components/ImageCard'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
+
+
+
+
 export default function OrdersScreen ({ navigation, route }) {
-  // done
+  //done
   const [orders, setOrders] = useState([])
   const { loggedInUser } = useContext(AuthorizationContext)
 
@@ -22,7 +26,7 @@ export default function OrdersScreen ({ navigation, route }) {
       const fetchedOrders = await getAll()
       setOrders(fetchedOrders)
     } catch (error) {
-      console.error('Error fetching orders:', error)
+      console.error('Error fetching orders:', error);
       showMessage({
         message: `There was an error while retrieving orders. ${error} `,
         type: 'error',
@@ -31,8 +35,8 @@ export default function OrdersScreen ({ navigation, route }) {
       })
     }
   }
-  // done
-
+  //done
+  
   useEffect(() => {
     if (loggedInUser) {
       fetchOrders()
@@ -40,8 +44,78 @@ export default function OrdersScreen ({ navigation, route }) {
       setOrders(null)
     }
   }, [loggedInUser, route])
-  // done
+  //done
+    
+  const renderOrder = ({ item }) => {
+    return (
+      //Imagen de restaurante
+      <ImageCard
+        imageUri={item.restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + item.restaurant.logo } : restaurantLogo}
+        title={item.restaurant.name}
+        onPress={() => {
+          navigation.navigate('OrderDetailScreen', { id: item.id })
+        }}
+      >
 
+        {/* Estatus de pedido */}
+        <TextRegular>Order placed on: <TextSemiBold>{new Date(item.createdAt).toLocaleString().replace(',','  ')}</TextSemiBold></TextRegular>
+
+        {/* Estatus de pedido */}
+        <TextRegular>Status: 
+          <TextSemiBold textStyle={item.status === 'in process' ? { color: GlobalStyles.brandSecondary } 
+          : item.status === 'sent' ? { color: GlobalStyles.brandGreen } 
+          : item.status === 'delivered' ? { color: GlobalStyles.brandBlue } 
+          : { color: GlobalStyles.brandPrimary }}>
+            {item.status}
+          </TextSemiBold>
+        </TextRegular>
+  
+        {/* Precio del pedido */}
+        <TextRegular>Price: <TextSemiBold>{item.price}€</TextSemiBold></TextRegular>
+  
+        {/* Modificaciones del pedido */}
+        {item.status === 'pending' &&//Renderizado condicional
+        <View style={styles.actionButtonsContainer}>
+          <Pressable
+           onPress={() => showMessage("TO DO") /* navigation.navigate('EditOrderScreen', { orderId: item.id, id: item.restaurant.id })*/}//TODO
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? GlobalStyles.brandBlueTap
+                  : GlobalStyles.brandGreen
+              },
+              styles.actionButton
+            ]}>
+            <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+              <MaterialCommunityIcons name='pencil' color={'white'} size={20}/>
+              <TextRegular textStyle={styles.text}>
+                Edit
+              </TextRegular>
+            </View>
+          </Pressable>
+  
+          <Pressable
+            onPress={() => { showMessage("TO DO") /*setOrderToBeDeleted(item)*/ }}//TODO
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? GlobalStyles.brandPrimaryTap
+                  : GlobalStyles.brandPrimary
+              },
+              styles.actionButton
+            ]}>
+            <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+              <MaterialCommunityIcons name='delete' color={'white'} size={20}/>
+              <TextRegular textStyle={styles.text}>
+                Delete
+              </TextRegular>
+            </View>
+          </Pressable>
+        </View>}
+  
+      </ImageCard>
+    )
+  }
   return (
     <View style={styles.container}>
       <View style={styles.FRHeader}>
@@ -59,8 +133,7 @@ export default function OrdersScreen ({ navigation, route }) {
 
       {/* Lista de pedidos */}
       <View style={styles.ordersContainer}>
-        { orders != null && orders.length > 0
-          ? (
+        { orders!= null && orders.length > 0 ? (
           <FlatList
             data={orders}
             renderItem={renderOrder}
@@ -68,115 +141,65 @@ export default function OrdersScreen ({ navigation, route }) {
             contentContainerStyle={styles.orderListContainer}
             horizontal={false}
           />
-            )
-          : (
+        ) : (
           <TextRegular>No orders available.</TextRegular>
-            )}
+        )}
       </View>
+
 
       <Pressable
         onPress={() => {
-          navigation.navigate('OrderDetailScreen', { id: Math.floor(Math.random() * 100) })
+          navigation.navigate('OrderDetailScreen', { id: Math.floor(Math.random() * 100) });
         }}
         style={({ pressed }) => [
           {
-            backgroundColor: pressed ? brandPrimaryTap : brandPrimary
+            backgroundColor: pressed ? brandPrimaryTap : brandPrimary,
           },
-          styles.button
+          styles.button,
         ]}
       >
         <TextRegular textStyle={styles.text}>Go to Order Detail Screen</TextRegular>
       </Pressable>
     </View>
-  )
+  );
 }
-const renderOrder = ({ item }) => {
-  return (
-    // Imagen de restaurante
-    <ImageCard
-      imageUri={item.restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + item.restaurant.logo } : restaurantLogo}
-      title={item.restaurant.name}
-      onPress={() => {
-        navigation.navigate('OrderDetailScreen', { id: item.id })
-      }}
-    >
-      {/* Estatus de pedido */}
-      <TextRegular>Status:
-        <TextSemiBold textStyle={item.status === 'in process'
-          ? { color: GlobalStyles.brandSecondary }
-          : item.status === 'sent'
-            ? { color: GlobalStyles.brandGreen }
-            : item.status === 'delivered'
-              ? { color: GlobalStyles.brandBlue }
-              : { color: GlobalStyles.brandPrimary }}>
-          {item.status}
-        </TextSemiBold>
-      </TextRegular>
 
-      {/* Precio del pedido */}
-      <TextRegular>Price: <TextSemiBold>{item.price}€</TextSemiBold></TextRegular>
-
-      {/* Modificaciones del pedido */}
-      {item.status === 'pending' &&// Renderizado condicional
-      <View style={styles.actionButtonsContainer}>
-        <Pressable
-         onPress={() => showMessage('TO DO') /* navigation.navigate('EditOrderScreen', { orderId: item.id, id: item.restaurant.id }) */}// TODO
-          style={({ pressed }) => [
-            {
-              backgroundColor: pressed
-                ? GlobalStyles.brandBlueTap
-                : GlobalStyles.brandGreen
-            },
-            styles.actionButton
-          ]}>
-          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
-            <MaterialCommunityIcons name='pencil' color={'white'} size={20}/>
-            <TextRegular textStyle={styles.text}>
-              Edit
-            </TextRegular>
-          </View>
-        </Pressable>
-
-        <Pressable
-          onPress={() => { showMessage('TO DO') /* setOrderToBeDeleted(item) */ }}// TODO
-          style={({ pressed }) => [
-            {
-              backgroundColor: pressed
-                ? GlobalStyles.brandPrimaryTap
-                : GlobalStyles.brandPrimary
-            },
-            styles.actionButton
-          ]}>
-          <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
-            <MaterialCommunityIcons name='delete' color={'white'} size={20}/>
-            <TextRegular textStyle={styles.text}>
-              Delete
-            </TextRegular>
-          </View>
-        </Pressable>
-      </View>}
-
-    </ImageCard>
-  )
-}
 const styles = StyleSheet.create({
   FRHeader: { // TODO: remove this style and the related <View>. Only for clarification purposes
     justifyContent: 'center',
     alignItems: 'left',
     margin: 50
   },
+  actionButtonsContainer:
+  {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row'
+
+  },
+  actionButton: 
+  {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+    alignItems: 'center',
+    borderRadius: 8,
+
+  },
   ordersContainer: {
     flex: 1,
     padding: 10,
-    marginBottom: 10,
+    marginBottom: 10, 
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: GlobalStyles.brandSeparator
+    borderColor: GlobalStyles.brandSeparator,
+    flex: 1,
   },
   orderListContainer: {
     flexGrow: 1,
     flexDirection: 'column',
-    alignItems: 'stretch'
+    alignItems: 'stretch', 
   },
   container: {
     flex: 1,
@@ -194,6 +217,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 16,
     color: 'white',
+    paddingLeft: 10,
     textAlign: 'center'
   }
 })
