d932f42ea5477a7ba0f46f635a894889adc2f51e
pabniecor <pabniecor@alum.us.es>
2024-05-19 12:32:19 +0200

    mensaje sobre envío gratis

--- DeliverUS-Frontend-Customer/src/screens/restaurants/RestaurantDetailScreen.js
+++ DeliverUS-Frontend-Customer/src/screens/restaurants/RestaurantDetailScreen.js
@@ -54,6 +54,10 @@ export default function RestaurantDetailScreen ({ navigation, route }) {
 
         {Object.keys(order).length > 0 &&
         <View style={styles.footerContainer}>
+          {getOrderSubTotal() <= 10 &&
+          <TextRegular>
+            {`Shipping costs are FREE if order price is higher than 10€`}
+          </TextRegular>}
           <TextSemiBold>
             {getOrderSubTotal() <= 10 ? `Shipping costs: ${restaurant.shippingCosts.toFixed(2)}€` : 'Free Shipping!'}
           </TextSemiBold>
